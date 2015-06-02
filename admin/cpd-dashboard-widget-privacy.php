72c80a0752e0fbe16dcbc5f4d1213ff4a566b656
Matt Watson <mattwatson@Matts-MacBook-Pro.local>
2015-06-02 17:01:38 +0100

    Restricted privacy widget

--- admin/cpd-dashboard-widget-privacy.php
+++ admin/cpd-dashboard-widget-privacy.php
@@ -57,8 +57,9 @@ class CPD_Dashboard_Widget_Privacy {
 	 * Add the dashboard widget
 	 */
 	public function add_dashboard_widget() {
-		
-		add_meta_box('cpd_dashboard_widget_welcome', '<span class="cpd-dashboard-widget-title dashicons-before dashicons-shield"></span> ' . 'Privacy', array( $this, 'render_dashboard_widget' ), 'dashboard', 'side', 'high' );
+		if( current_user_can( 'manage_options' ) ) {
+			add_meta_box('cpd_dashboard_widget_welcome', '<span class="cpd-dashboard-widget-title dashicons-before dashicons-shield"></span> ' . 'Privacy', array( $this, 'render_dashboard_widget' ), 'dashboard', 'side', 'high' );
+		}
 	}
 
 	/**
